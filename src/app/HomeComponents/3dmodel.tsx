a4967bec9f48fa4af5e047b13f7e0ac9a2ecc4e7
Ravinder Singh Bisht <ravinderbisht5691@gmail.com>
2025-05-05 00:54:06 +0530

    add 3d spline model

---
+++ src/app/HomeComponents/3dmodel.tsx
@@ -0,0 +1,16 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Model() {
+    return (
+        <main className='flex flex-col place-items-center justify-center h-screen'>
+            <Spline
+                scene="https://prod.spline.design/GJ6cSezNyXe5qF-D/scene.splinecode"
+                
+            />
+            <Spline
+                scene="https://prod.spline.design/F8zUWoAhZqILom1y/scene.splinecode"
+                
+            />
+        </main>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ import Faq from "./HomeComponents/faqs";
 import KeyFeatures from "./HomeComponents/features";
 import Footer from "./HomeComponents/footer";
 import Navbar from "./navbar/homeNav";
+import Model from "./HomeComponents/3dmodel";
 // import Partners from "./(components)/partersBar/page";
 
 export default function Home() {
   return (
     <div className="px-20">
       <Navbar />
-      <div className="flex item-center justify-between my-30 gap-5">
+      <div className="flex item-center justify-between my-20 max-h-screen gap-5">
         <div className="flex flex-col flex-1 justify-center gap-10">
           <h1 className="text-[40px] tracking-wide leading-13 font-semibold">Your AI-Powered Guide to <span className="text-transparent  bg-clip-text bg-[linear-gradient(90deg,#A07DF1,#F69DBA)] ">Education</span>, <span className="text-transparent  bg-clip-text bg-[linear-gradient(90deg,#A07DF1,#F69DBA)]">Careers</span> & <span className="text-transparent  bg-clip-text bg-[linear-gradient(90deg,#A07DF1,#F69DBA)]">Everything</span> in Between</h1>
           <span className="text-left w-3/4">Alumna.ai is India’s first dedicated AI foundation model for EdTech — <br />
@@ -21,14 +22,8 @@ export default function Home() {
             <InputBar />
           </div>
         </div>
-        <div className="">
-          <Image
-            src="/Image/robot.png" // Assuming the image is located at public/images/hero-image.jpg
-            alt="Hero Image"
-            width="600"
-            height="500"
-            className="rounded-lg shadow-lg"
-          />
+        <div className=" flex flex-1 justify-end">
+          <Model/>
         </div>
       </div>
 
